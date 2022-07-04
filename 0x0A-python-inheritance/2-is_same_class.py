38dfe866166016265daf1aba96eb8773790f69a6
Angiephoenix <angiephoenix93@gmail.com>
2022-07-04 16:51:59 -0700

    python-inheritance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""2-i_same"""
+
+
+def is_same_class(obj, a_class):
+    """if object is an istanace of a class
+    args:
+    object being
+    class attribute
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+se_geometry: class BaseGeometry
+"""
+class BaseGeometry:
+    """
+        An empty class.
+    """
+    pass
